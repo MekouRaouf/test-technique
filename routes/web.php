2020df38fe4b1734ea8bcafa7574be200ab22dcc
Mekou Raouf <raoufmekou@gmail.com>
2022-09-05 16:24:58 +0200

    Added Evenement CRUD routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EvenementController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -16,20 +17,16 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Welcome', [ 'canLogin' => Route::has('login'), 'canRegister' => Route::has('register'), 'laravelVersion' => Application::VERSION, 'phpVersion' => PHP_VERSION ]);
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+Route::middleware([ 'auth:sanctum', config('jetstream.auth_session'), 'verified' ])->group(function () {
+    Route::get('/dashboard', function () { return Inertia::render('Dashboard'); })->name('dashboard');
+    Route::prefix('evenements')->group(function(){
+        Route::get('/', [EvenementController::class, 'index'])->name('evenements');
+        Route::get('/create', [EvenementController::class, 'store'])->name('evenement.store');
+        Route::get('/{evenement}/edit', [EvenementController::class, 'edit'])->name('evenements.edit');
+        Route::get('/{evenement}/delete', [EvenementController::class, 'destroy'])->name('evenements.delete');
+        Route::get('/{evenement}/restore', [EvenementController::class, 'restore'])->name('evenements.restore');
+    });
 });
